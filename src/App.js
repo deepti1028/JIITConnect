019de3f8d0df5c5bb039f92919dce2872301bf13
Dipti Jain <deeptihnd000@gmail.com>
2023-10-31 22:21:09 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {useState} from "react";
 import './App.css';
+import "./style.scss";
+import "./media-query.css";
 import Home from './pages/Home';
+import Detail from './pages/Detail';
+import AddEditBlog from './pages/AddEditBlog';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
+
 function App() {
+  const [active, setActive] = useState("home");
   return (
     <div className="App">
-      <Home />
+      <Header setActive={setActive} active={active}/>
+      <ToastContainer />
+     <Routes >
+      <Route path="/" element={<Home />} />
+      <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/create" element={<AddEditBlog />} />
+      <Route path="/update/:id" element={<AddEditBlog />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+     </Routes>
+
     </div>
   );
 }
