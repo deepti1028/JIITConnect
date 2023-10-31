e3a981f86844833b1a8bc68d439642a6cfc8766d
Riya Sumra <sumrariya@gmail.com>
2023-11-01 00:01:27 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import './App.css';
 import "./style.scss";
 import "./media-query.css";
@@ -14,16 +14,44 @@ import Header from './components/Header';
 
 function App() {
   const [active, setActive] = useState("home");
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate(); 
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      setUser(null);
+      setActive("login");
+      navigate("/auth");
+    });
+  };
+
   return (
     <div className="App">
-      <Header setActive={setActive} active={active}/>
-      <ToastContainer />
+      <Header 
+      setActive={setActive} 
+      active={active} 
+      user = {user} 
+      handleLogout= {handleLogout}
+      />
+      <ToastContainer position= "top-center" />
      <Routes >
       <Route path="/" element={<Home />} />
       <Route path="/detail/:id" element={<Detail />} />
       <Route path="/create" element={<AddEditBlog />} />
       <Route path="/update/:id" element={<AddEditBlog />} />
       <Route path="/about" element={<About />} />
+      <Route path="/auth" element={<Auth setActive= {setActive} />} />
       <Route path="*" element={<NotFound />} />
      </Routes>
 
