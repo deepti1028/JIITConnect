c70611c6580c77378b8fde9508ba63bd34ca6829
Dipti Jain <deeptihnd000@gmail.com>
2023-11-05 01:21:47 +0530

    created trending section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,47 @@
-import { collection, deleteDoc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  onSnapshot,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
 import BlogSection from "../components/BlogSection";
 import Spinner from "../components/Spinner";
-import { doc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import Tags from "../components/Tags";
 import MostPopular from "../components/MostPopular";
+import Trending from "../components/Trending";
 const Home = ({ setActive, user }) => {
   const [loading, setLoading] = useState(true);
   const [blogs, setBlogs] = useState([]);
   const [tags, setTags] = useState([]);
+  const [trendBlogs, setTrendBlogs] = useState([]);
+
+  const getTrendingBlog = async () => {
+    const blogRef = collection(db, "blogs");
+    const trendQuery = query(blogRef, where("trending", "==", "yes"));
+    const querySnapshot = await getDocs(trendQuery);
+    let trendBlogs = [];
+    querySnapshot.forEach((doc) => {
+      trendBlogs.push({ id: doc.id, ...doc.data() });
+    });
+    setTrendBlogs(trendBlogs);
+  };
 
   useEffect(() => {
+    getTrendingBlog();
     const unsub = onSnapshot(
       collection(db, "blogs"),
       (snapshot) => {
         let list = [];
-        let tags= [];
+        let tags = [];
         snapshot.docs.forEach((doc) => {
           tags.push(...doc.get("tags"));
-          list.push({ id: doc.id, ...doc.data() })
+          list.push({ id: doc.id, ...doc.data() });
         });
         const uniqueTags = [...new Set(tags)];
         setTags(uniqueTags);
@@ -34,6 +55,7 @@ const Home = ({ setActive, user }) => {
     );
     return () => {
       unsub();
+      getTrendingBlog();
     };
   }, []);
 
@@ -48,7 +70,7 @@ const Home = ({ setActive, user }) => {
         await deleteDoc(doc(db, "blogs", id));
         toast.success("Blog deleted successfully");
         setLoading(false);
-      } catch(err) {
+      } catch (err) {
         console.log(err);
       }
     }
@@ -60,7 +82,7 @@ const Home = ({ setActive, user }) => {
     <div className="container-fluid pb-4 pt-4 padding">
       <div className="container padding">
         <div className="row mx-0">
-          <h2>Trending</h2>
+          <Trending blogs={trendBlogs} />
           <div className="col-md-8">
             <BlogSection
               blogs={blogs}
@@ -69,8 +91,8 @@ const Home = ({ setActive, user }) => {
             />
           </div>
           <div className="col-md-3">
-           <Tags tags={tags} />
-           <MostPopular blogs={blogs}/>
+            <Tags tags={tags} />
+            <MostPopular blogs={blogs} />
           </div>
         </div>
       </div>
