5d67b5281374e973dd4294d9c666e3d028534083
Dipti Jain <deeptihnd000@gmail.com>
2023-11-05 01:28:57 +0530

    created about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-       <h2>About</h2>
+    <div className="containter padding">
+      <div className="col-md-12">
+        <div className="row mx-0">
+          <p>
+            DUMMY CONTENT
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
